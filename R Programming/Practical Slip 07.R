a696f5e6bf9065ac065d5303c1f0a3cfd4aaadc9
Atharva0506 <atharvan.coder@gmail.com>
2022-11-08 14:00:53 +0530

    Updating R programing Slips

---
+++ R Programming/Practical Slip 07.R
@@ -0,0 +1,10 @@
+#Write an R program to create a Dataframe which contain details of 5 employees and display the details in ascending order.
+Employees = data.frame(
+  Name=c("Virat", "Rohit", "Atharva", "Ronaldo", "Sam"),
+  Gender=c("M","M","F","M","F"),
+  Age=c(23,22,25,26,32),
+  Designation=c("Clerk", "Manager", "Executive", "CEO", "ASSISTANT"),
+  Phone No=c("123-34-2346","123-44-779","556-24-433","123-98-987","679-77-576")
+)
+
+Employees
