2d08a8472cad61bb9270e9dad6738dd1b369d07c
Atharva Naik <atharvan.coder@gmail.com>
2023-02-22 21:52:18 +0530

    Delete R Programming directory

--- R Programming/Slip19.r
+++
@@ -1,7 +0,0 @@
-# Slip 19
-# Write an R program to draw an empty plot and an empty plot
-# specify the axes limits of the graphic.
-#print("Empty plot:")
-plot.new()
-#print("Empty plot specify the axes limits of the graphic:")
-plot(1, type="n", xlab="", ylab="", xlim=c(0, 20), ylim=c(0, 20))
--- R Programming/Slip27.r
+++
@@ -1,8 +0,0 @@
-# Slip 27
-# Write an R Program to calculate binary into Decimal of a given number.
-install.packages("asbio")
-library(asbio)
-bin2dec(1010)
-#asbio: - is Package, Contains functions from, Foundational Statistics and Applied Analyses for
-#Biologists.
-#bin2dec( )- This function is used to convert decimal values to binary values
